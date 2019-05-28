c0bb9c75853ef5e4d2027c2e881dd042e66bd1cb
Kevin Le Run <lerunkevin@gmail.com>
2019-05-28 15:32:22 +0200

    Now using the zig build system

---
+++ build.zig
@@ -0,0 +1,21 @@
+const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    var exe = b.addExecutable("blink", "src/main.zig");
+    exe.setBuildMode(mode);
+
+    exe.addIncludeDir("/usr/include");
+
+    exe.linkSystemLibrary("c");
+    exe.linkSystemLibrary("SDL2");
+    exe.linkSystemLibrary("SDL2_image");
+
+    exe.setOutputDir(".");
+
+    b.default_step.dependOn(&exe.step);
+
+    b.installArtifact(exe);
+}
