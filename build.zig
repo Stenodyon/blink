6c6d70e47281567ef0a0c4128a5fc6cb1a3bbb21
Stenodyon <stenodyon@gmail.com>
2019-12-07 14:35:43 +0100

    build.zig automatic packaging for release

--- build.zig
+++ build.zig
@@ -1,44 +1,67 @@
-const Builder = @import("std").build.Builder;
+const std_build = @import("std").build;
+const Builder = std_build.Builder;
+const LibExeObjStep = std_build.LibExeObjStep;
 const builtin = @import("builtin");
 
 const CFLAGS = [_][]const u8{"-O2"};
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const windows = b.option(
-        bool,
-        "windows",
-        "cross-compile to Microsoft Windows",
-    ) orelse false;
+    const windows_step = b.step("windows", "Cross-compile to Microsoft Windows");
+    const package_step = b.step("package", "Package binaries for release");
 
+    var exe_linux = build_linux(b);
+    var exe_windows = build_windows(b);
+
+    b.default_step.dependOn(&exe_linux.step);
+    var install_windows = b.addInstallArtifact(exe_windows);
+    windows_step.dependOn(&install_windows.step);
+
+    var package_script = b.addSystemCommand([_][]const u8{"./package.sh"});
+    package_script.step.dependOn(&exe_linux.step);
+    package_script.step.dependOn(&exe_windows.step);
+    package_step.dependOn(&package_script.step);
+
+    b.installArtifact(exe_linux);
+}
+
+fn build_common(b: *Builder) *LibExeObjStep {
+    const mode = b.standardReleaseOptions();
     var exe = b.addExecutable("blink", "src/main.zig");
     exe.setBuildMode(mode);
     exe.addIncludeDir("third-party/lodepng/include");
     exe.addCSourceFile("third-party/lodepng/src/lodepng.c", CFLAGS[0..]);
-    if (windows) {
-        exe.setTarget(
-            builtin.Arch.x86_64,
-            builtin.Os.windows,
-            builtin.Abi.gnu,
-        );
-        exe.addIncludeDir("/usr/include");
-
-        exe.addObjectFile("lib/libSDL2main.a");
-        exe.addObjectFile("lib/libSDL2.dll.a");
-        exe.addObjectFile("lib/libSDL2_ttf.dll.a");
-        exe.addObjectFile("lib/libopengl32.a");
-        exe.addObjectFile("lib/epoxy.lib");
-    } else {
-        exe.linkSystemLibrary("SDL2");
-        exe.linkSystemLibrary("SDL2_ttf");
-        exe.linkSystemLibrary("GL");
-        exe.linkSystemLibrary("epoxy");
-    }
     exe.linkSystemLibrary("c");
-
     exe.setOutputDir(".");
 
-    b.default_step.dependOn(&exe.step);
+    return exe;
+}
 
-    b.installArtifact(exe);
+fn build_linux(b: *Builder) *LibExeObjStep {
+    var exe = build_common(b);
+
+    exe.linkSystemLibrary("SDL2");
+    exe.linkSystemLibrary("SDL2_ttf");
+    exe.linkSystemLibrary("GL");
+    exe.linkSystemLibrary("epoxy");
+
+    return exe;
+}
+
+fn build_windows(b: *Builder) *LibExeObjStep {
+    var exe = build_common(b);
+
+    exe.setTarget(
+        builtin.Arch.x86_64,
+        builtin.Os.windows,
+        builtin.Abi.gnu,
+    );
+    exe.addIncludeDir("/usr/include");
+
+    exe.addObjectFile("deps/windows/lib/libSDL2main.a");
+    exe.addObjectFile("deps/windows/lib/libSDL2.dll.a");
+    exe.addObjectFile("deps/windows/lib/libSDL2_ttf.dll.a");
+    exe.addObjectFile("deps/windows/lib/libopengl32.a");
+    exe.addObjectFile("deps/windows/lib/epoxy.lib");
+
+    return exe;
 }
---
+++ package.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+TMP_DIR=/tmp/blink
+
+set -e
+
+mkdir -p $TMP_DIR
+
+if [ ! -f "$TMP_DIR/SDL2.zip" ]; then
+    wget "https://www.libsdl.org/release/SDL2-2.0.10-win32-x64.zip" -O $TMP_DIR/SDL2.zip
+fi
+if [ ! -f "$TMP_DIR/SDL2_ttf.zip" ]; then
+    wget "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15-win32-x64.zip" -O $TMP_DIR/SDL2_ttf.zip
+fi
+if [ ! -f "$TMP_DIR/libepoxy.zip" ]; then
+    wget "https://github.com/anholt/libepoxy/releases/download/1.5.3/libepoxy-shared-x64.zip" -O $TMP_DIR/libepoxy.zip
+fi
+
+unzip -o $TMP_DIR/SDL2.zip -d $TMP_DIR
+unzip -o $TMP_DIR/SDL2_ttf.zip -d $TMP_DIR
+unzip -oj $TMP_DIR/libepoxy libepoxy-shared-x64/bin/epoxy-0.dll -d $TMP_DIR
+
+mkdir -p $TMP_DIR/Blink_Windows_x64
+mkdir -p $TMP_DIR/Blink_Linux_x64
+
+cp blink README.md $TMP_DIR/Blink_Linux_x64/
+cp -r data $TMP_DIR/Blink_Linux_x64/
+
+cp blink.exe README.md $TMP_DIR/Blink_Windows_x64/
+cp -r data $TMP_DIR/Blink_Windows_x64/
+cp $TMP_DIR/*.dll $TMP_DIR/Blink_Windows_x64/
+
+rm -f Blink_Linux_x64.zip
+rm -f Blink_Windows_x64.zip
+
+zip -r Blink_Linux_x64.zip $TMP_DIR/Blink_Linux_x64
+zip -r Blink_Windows_x64.zip $TMP_DIR/Blink_Windows_x64
